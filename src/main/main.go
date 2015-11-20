5009f2f987b8779026d17c31d3ecfb0b8019ff4c
Richard van der Hoff <richard@matrix.org>
2015-11-20 12:22:39 +0000

    Make a /sync request on connection, and pipe it back down the tube.

--- src/main/main.go
+++ src/main/main.go
@@ -3,12 +3,17 @@ package main
 import (
   "fmt"
   "github.com/gorilla/websocket"
+  "io"
+  "io/ioutil"
   "log"
   "net/http"
+  "net/url"
   "time"
 )
 
 const (
+	listenPort = 8009
+
 	// Time allowed to write a message to the peer.
 	writeWait = 10 * time.Second
 
@@ -20,47 +25,115 @@ const (
 
 	// Maximum message size allowed from peer.
 	maxMessageSize = 512
+	
+	upstreamUrl = "http://localhost:8008/_matrix/client/v2_alpha/sync"
 )
 
 
 func main() {
-	fmt.Println("Starting websock server")
+	fmt.Println("Starting websock server on port", listenPort)
 	http.Handle("/test/", http.StripPrefix("/test/", http.FileServer(http.Dir("test"))))
-	http.HandleFunc("/ws", serveWs)
-	err := http.ListenAndServe(":8800", nil)
+	http.HandleFunc("/stream", serveStream)
+	err := http.ListenAndServe(fmt.Sprintf(":%v", listenPort), nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
   	}
 }
 
-var upgrader = websocket.Upgrader{
-	Subprotocols: []string{"m.json"},
+func httpError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
 }
 
-func serveWs(w http.ResponseWriter, r *http.Request) {
+// handle a request to /stream
+//
+func serveStream(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Got websocket request to", r.URL)
+	
+	if r.Method != "GET" {
+		log.Println("Invalid method", r.Method)
+		httpError(w, http.StatusMethodNotAllowed)
+		return
+	}
+	
+	c := newConnection(r)
+	resp, err := c.sync()
+	if err != nil {
+		log.Println("Error in sync", err)
+		httpError(w, http.StatusInternalServerError)
+		return
+	}
+	
+	defer resp.Body.Close()
+	fmt.Println("Sync response:", resp.StatusCode)
+	if resp.StatusCode != 200 {
+		// send the error back to the client
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Println("Error reading sync error response", err)
+			httpError(w, http.StatusInternalServerError)
+			return
+		}
+		log.Println("sync failed:", string(body), resp.Header)
+		w.Header().Set("Content-Type", resp.Header.Get("Content-Type"))
+		w.WriteHeader(resp.StatusCode)
+		w.Write(body)
+		return
+	}
+	
+	upgrader := websocket.Upgrader {
+		Subprotocols: []string{"m.json"},
+	}
+
 	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
-		return
 	}
-	c := &connection{send: make(chan []byte, 256), ws: ws}
+	c.ws = ws
+	defer c.ws.Close()
+
+	go c.syncPump(resp.Body)
 	go c.writePump()
 	c.reader()
 }
 
-type connection struct {
-	// The websocket connection.
+
+type matrixConnection struct {
+	// The websocket connection. nil until we upgrade the connection.
 	ws *websocket.Conn
 
+	// our client for the upstream connection
+	client *http.Client
+
+	syncParams url.Values 
+
 	// Buffered channel of outbound messages.
-	send chan []byte
+	messageSend chan []byte
+	
+	// Channel of sync responses
+	syncSend chan io.ReadCloser
 }
 
-func (c *connection) reader() {
-	defer func() {
-		c.ws.Close()
-	}()
+// create a new matrixConnection for the received request
+func newConnection(r *http.Request) *matrixConnection {
+	result := &matrixConnection{
+		syncParams: r.URL.Query(),
+		messageSend: make(chan []byte, 256),
+		syncSend: make(chan io.ReadCloser),
+	}
+
+	result.client = &http.Client{} 
+
+	return result
+}
+
+func (c *matrixConnection) sync() (*http.Response, error) {
+	url := upstreamUrl + "?" + c.syncParams.Encode()
+		 
+	fmt.Println("Upstream URL:", url)
+	return c.client.Get(url)
+}
+
+func (c *matrixConnection) reader() {
 	c.ws.SetReadLimit(maxMessageSize)
 	c.ws.SetReadDeadline(time.Now().Add(pongWait))
 	c.ws.SetPongHandler(func(string) error { c.ws.SetReadDeadline(time.Now().Add(pongWait)); return nil })
@@ -70,32 +143,66 @@ func (c *connection) reader() {
 			break
 		}
 		fmt.Println("Got message: ", string(message))
-		c.send <- []byte("test response")
 	}
 	fmt.Println("Closing socket")
 }
 
+
+func (c *matrixConnection) syncPump(initialResponse io.ReadCloser) {
+	c.syncSend <- initialResponse
+}
+
+
+func (c *matrixConnection) writeSync(syncResponse io.ReadCloser) error {
+	defer syncResponse.Close()
+	
+	c.ws.SetWriteDeadline(time.Now().Add(writeWait))
+
+	wr, err := c.ws.NextWriter(websocket.TextMessage)
+	if err != nil {
+		return err
+	}
+	defer wr.Close()
+	if _, err := io.Copy(wr, syncResponse); err != nil {
+		return err
+	}
+	return nil
+}
+
+
 // write writes a message with the given message type and payload.
-func (c *connection) write(mt int, payload []byte) error {
+func (c *matrixConnection) write(mt int, payload []byte) error {
 	c.ws.SetWriteDeadline(time.Now().Add(writeWait))
 	return c.ws.WriteMessage(mt, payload)
 }
 
-// writePump pumps messages out to the websocket connection.
-func (c *connection) writePump() {
+func (c *matrixConnection) writeClose() error {
+	return c.write(websocket.CloseMessage, []byte{})
+}
+
+
+// writePump pumps messages out to the websocket connection, and takes
+// responsibility for sending pings.
+func (c *matrixConnection) writePump() {
+	defer c.ws.Close()
 	ticker := time.NewTicker(pingPeriod)
-	defer func() {
-		ticker.Stop()
-		c.ws.Close()
-	}()
+	defer ticker.Stop()
 	for {
 		select {
-		case message, ok := <-c.send:
+		case message, ok := <- c.messageSend:
 			if !ok {
-				c.write(websocket.CloseMessage, []byte{})
+				c.writeClose()
 				return
 			}
 			if err := c.write(websocket.TextMessage, message); err != nil {
+				return
+			}
+		case reader, ok := <- c.syncSend:
+			if !ok {
+				c.writeClose()
+				return
+			}
+			if err := c.writeSync(reader); err != nil {
 				return
 			}
 		case <-ticker.C:
--- test/test.js
+++ test/test.js
@@ -1,9 +1,37 @@
-var socket = new WebSocket("ws://localhost:8800/ws", "m.json");
+var socket;
+
+function start() {
+    if(socket) {
+        socket.close();
+        socket = null;
+    }
+    clear_log();
+    
+    var access_token = document.getElementById("token").value;
+    var url = "ws://"+window.location.host+"/stream" +
+        "?access_token="+encodeURIComponent(access_token);
+
+    socket = new WebSocket(url, "m.json");
+    socket.onopen = function(ev) {
+        console.log("Connected to "+url);
+    }
+    socket.onmessage = append_message;
+}
+
+function clear_log() {
+    $("#log").empty();
+}
+
+function append_message(ev) {
+    console.log("Got message "+ev.data)
+    new_elem = $("<div/>").text(ev.data)
+    new_elem.appendTo($("#log"))
+}
 
 function send(ev) {
     var msg = {
         type: "message",
-        text: document.getElementById("text").value,
+        text: $("#text").value,
         date: Date.now()
     };
 
