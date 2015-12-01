1b2fa0549ef6b5bf9b0ecfceb22536499c104e80
Richard van der Hoff <richard@matrix.org>
2015-12-01 12:11:48 +0000

    Shell script for jenkins job

---
+++ jenkins.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -ex
+
+export GOPATH=`pwd`/gopath
+cd $GOPATH/src/github.com/matrix-org/matrix-websockets-proxy
+go get -v
+go build
+go test ./proxy
+
+unformatted=$(find . -name '*.go' -print0 | xargs -0 gofmt -l)
+[ -z "$unformatted" ] || {
+    echo "Unformatted files:"
+    echo $unformatted
+    exit 1
+}
